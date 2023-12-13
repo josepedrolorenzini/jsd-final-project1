f0cb5a1560e2178b7bd1f7512a7c8d8f9b350708
joseplorenzini <joseplorenzini@gmail.com>
2023-12-13 15:57:22 +1100

    delete

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Button(props) {
-  return (
-    <button className="favorite styled" type="button" onClick={props.handleclick} >Display Characters</button>
-  )
-}
-
-export default Button
